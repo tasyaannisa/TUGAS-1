eba2a0ba5eab496c594a4dad37f5022913e0824a
tasyaannisa <48597113+tasyaannisa@users.noreply.github.com>
2019-03-23 19:16:42 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+html>
+    <head>
+      <title> information retrieval </title>
+    <style>
+h1 {
+	color:black;
+	text-align: center ;
+	margin-top :10%;
+  }
+  </style>
+  </head>
+  <body>
+  <h1> <?php echo 'hello world <br>';
+echo 'TASYA ANNISA ANGGRAINI 16.01.55.0047';  ?>
+		
+	
+	</h1>
+        
+  </body>
+  </html>
